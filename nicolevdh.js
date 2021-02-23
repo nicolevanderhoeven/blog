c5ade6ebb13b163b30ba9fa17a8b5fb2ff960c84
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2021-02-23 14:46:26 +0100

    Added load test and workflow

---
+++ nicolevdh.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    duration: '10m',
+    vus: 50,
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+    },
+};
+
+export default function () {
+    let res = http.get('https://nicolevanderhoeven.com', {tags: { name: '01_Home' }});
+    check(res, {
+      'is status 200': (r) => r.status === 200,
+      'text verification': (r) => r.body.includes("Developer Advocate at k6.io")
+    });
+    sleep(Math.random() * 5);
+}
