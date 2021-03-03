01aaf34800afc61188acb54380c0888dc5c5298f
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2021-03-03 22:22:38 +0100

    Modified k6 script to add embedded resources

--- nicolevdh.js
+++ nicolevdh.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { sleep, check, group } from 'k6';
 
 export const options = {
     duration: '5m',
@@ -9,11 +9,61 @@ export const options = {
     },
 };
 
+const domain = 'https://nicolevanderhoeven.com';
 export default function () {
-    let res = http.get('https://nicolevanderhoeven.com', {tags: { name: '01_Home' }});
-    check(res, {
-      'is status 200': (r) => r.status === 200,
-      'text verification': (r) => r.body.includes("Developer Advocate at k6.io")
-    });
+    Home();
+}
+
+export function Home () {
+
+    group('Go to home page', function () {
+        let res = http.get(domain);
+        check(res, {
+        'is status 200': (r) => r.status === 200,
+        'text verification': (r) => r.body.includes("Developer Advocate at k6.io")
+        });
+
+        res = http.get(domain + '/lib/font-awesome/css/all.min.css');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+
+        res = http.get(domain + '/lib/jquery/jquery.min.js');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+
+        res = http.get(domain + '/js/main.js');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+
+        res = http.get(domain + '/css/style-white.css');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+
+        res = http.get(domain + '/images/logo.png');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+        
+        res = http.get(domain + '/lib/JetBrainsMono/ttf/JetBrainsMono-Regular.ttf');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+
+        res = http.get(domain + '/lib/font-awesome/webfonts/fa-brands-400.woff2');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+
+        res = http.get(domain + '/images/favicon.ico');
+        check(res, {
+        'is status 200': (r) => r.status === 200
+        });
+    })
+    
+
     sleep(Math.random() * 5);
 }
