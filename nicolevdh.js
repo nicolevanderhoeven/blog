143b52152544bac572af3aed0d4e1745be41d8f7
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2021-05-10 20:16:26 +0200

    Added Obsidian notes to test

--- nicolevdh.js
+++ nicolevdh.js
@@ -1,19 +1,22 @@
 import http from 'k6/http';
 import { sleep, check, group } from 'k6';
 
-export const options = {
-    duration: '5m',
-    vus: 5,
-    thresholds: {
-        http_req_duration: ['p(95)<500'],
-        http_req_failed: ['rate<0.05'],
-        checks: ['rate>0.99'],
-    },
-};
+// export const options = {
+//     duration: '5m',
+//     vus: 5,
+//     thresholds: {
+//         http_req_duration: ['p(95)<500'],
+//         http_req_failed: ['rate<0.05'],
+//         checks: ['rate>0.99'],
+//     },
+// };
 
 const domain = 'https://nicolevanderhoeven.com';
 export default function () {
     Home();
+    ThinkTime();
+    Notes();
+    ThinkTime();
 }
 
 export function Home () {
@@ -21,51 +24,27 @@ export function Home () {
     group('Go to home page', function () {
         let res = http.get(domain);
         check(res, {
-        'is status 200': (r) => r.status === 200,
-        'text verification': (r) => r.body.includes("Developer Advocate at k6.io")
+        'Homepage text verification': (r) => r.body.includes("Developer Advocate at k6.io")
         });
 
-        res = http.get(domain + '/lib/font-awesome/css/all.min.css');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
+        http.get(domain + '/lib/font-awesome/css/all.min.css');
+        http.get(domain + '/lib/jquery/jquery.min.js');
+        http.get(domain + '/js/main.js');
+        http.get(domain + '/css/style-white.css');
+        http.get(domain + '/images/logo.png');
+        http.get(domain + '/lib/JetBrainsMono/ttf/JetBrainsMono-Regular.ttf');
+        http.get(domain + '/lib/font-awesome/webfonts/fa-brands-400.woff2');
+        http.get(domain + '/images/favicon.ico');
+    });
+}
 
-        res = http.get(domain + '/lib/jquery/jquery.min.js');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
+export function Notes () {
+    let res = http.get("https://notes.nicolevanderhoeven.com");
+    check(res, {
+        'Notes page text verification': (r) => r.body.includes("my working notes")
+    });
+}
 
-        res = http.get(domain + '/js/main.js');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
-
-        res = http.get(domain + '/css/style-white.css');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
-
-        res = http.get(domain + '/images/logo.png');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
-        
-        res = http.get(domain + '/lib/JetBrainsMono/ttf/JetBrainsMono-Regular.ttf');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
-
-        res = http.get(domain + '/lib/font-awesome/webfonts/fa-brands-400.woff2');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
-
-        res = http.get(domain + '/images/favicon.ico');
-        check(res, {
-        'is status 200': (r) => r.status === 200
-        });
-    })
-    
-
+export function ThinkTime() {
     sleep(Math.random() * 5);
 }
